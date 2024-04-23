cda05aeaa27cacd7a5c4d44d1fd988bfaacea618
kaisoku2416 <kaisoku2416@gmail.com>
2024-04-24 02:29:34 +0900

    タブ幅を変更

--- app/controllers/fibonacci_controller.rb
+++ app/controllers/fibonacci_controller.rb
@@ -1,23 +1,23 @@
 class FibonacciController < ApplicationController
-	def show
-		begin
-			raise StandardError if !params[:n]
-			raise StandardError if params[:n] !~ /^[0-9]+$/
-			number = params[:n].to_i
-			result = fibonacci_calc(number)
-			render json: { result: result }
-		rescue
-			render json: { status: 400, message: "Bad request."}
-		end
+  def show
+    begin
+      raise StandardError if !params[:n]
+      raise StandardError if params[:n] !~ /^[0-9]+$/
+      number = params[:n].to_i
+      result = fibonacci_calc(number)
+      render json: { result: result }
+    rescue
+      render json: { status: 400, message: "Bad request."}
+    end
   end
 
   private
   def fibonacci_calc(n)
     a,b,c=1,1,1
-    (n-2).times do
-			c=a+b
-      a,b=b,c
-    end
+      (n-2).times do
+        c=a+b
+        a,b=b,c
+      end
     c
-	end
+  end
 end
